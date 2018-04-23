ae6d9f3d3b5778e4f50df137316bb2c2ae6b95d9
tvio <tvio@centrum.cz>
2018-04-23 18:07:52 +0200

    add yargs

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+ .options({
+  a: {
+      demanded: true,
+      alias: 'address',
+      describe: 'Address to fetch weather app',
+      string: true
+     }
+ })
+ .help()
+ .alias('help','h')
+ .argv;
+
+console.log(argv);
+
+var encodeAddr = encodeURIComponent(argv.address);
+console.log (encodeAddr);
 
 request({
-    url: 'https://maps.google.com/maps/api/geocode/json?address=Prague,Czech%20Republic&key=AIzaSyBsz7ns_gXieBPQhuGw0XqV758PXzjUngc',
+    url: `https://maps.google.com/maps/api/geocode/json?address=${encodeAddr}&key=AIzaSyBsz7ns_gXieBPQhuGw0XqV758PXzjUngc`,
     json: true
 },(err,res, body)=>{
     console.log(`Address:${body.results[0].formatted_address}`);
